1ab2605327aad641e99ca39b448b21029bad50fb
Cixot777 <cixot777@gmail.com>
2017-02-03 20:13:46 +0200

    2

--- students/views.py
+++ students/views.py
@@ -1,8 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#Views for Students
+
 def students_list(request):
-	return render(request, 'students/students_list.html',{})
+	students = (
+		{'id': 1,
+		'first_name': u'Віталій',
+		'last_name': u'Подоба',
+		'ticket':235,
+		'image': 'img/Enot.jpg'},
+		{'id': 2,
+		'first_name': u'Корост',
+		'last_name': u'Андрій',
+		'ticket':2123,
+		'image': 'img/Vovk.jpg'},
+		{'id': 3,
+		'first_name': u'Гавриленко',
+		'last_name': u'Горел',
+		'ticket':1444,
+		'image': 'img/Gorel.jpg'},
+		)
+	return render(request, 'students/students_list.html',{'students':students})
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
 def students_edit(request, sid):
